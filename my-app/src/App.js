c2df8d8b7634116198da76bb6d6a889dbdfe357e
Malinsha Vithanage <86399536+MaliVitha@users.noreply.github.com>
2023-08-17 12:01:37 +0530

    implemented routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./index.css";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Project from "./routes/Project";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div >
-    <h1>This is App.js
-    </h1>
-    </div>
+<>
+<Routes>
+  <Route path="/"n element={<Home/>}/>
+  <Route path="/project"n element={<About/>}/>
+  <Route path="/about"n element={<Project/>}/>
+  <Route path="/contact"n element={<Contact/>}/>
+  
+  
+</Routes>
+</>
   );
 }
 
