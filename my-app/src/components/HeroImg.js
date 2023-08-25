006af70f2fc1728626d0acc1a7d49734dacffc65
Malinsha Vithanage <kavindigvm.20@uom.lk>
2023-08-25 15:01:14 +0530

    home section 1 finished

---
+++ my-app/src/components/HeroImg.js
@@ -0,0 +1,27 @@
+import "./HeroImgStyles.css";
+
+import React from 'react'
+import IntroImg from "../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
+const HeroImg = () => {
+  return (
+    <div className="hero">
+    <div className="mask">
+      <img className="into-img" src={ IntroImg } alt="IntroImg"/>
+    </div>
+    <div className="content">
+        <p>HI, I'M MALINSHA VITHANAGE. </p>
+        <h1>Tech Enthusiast.</h1>
+        <div>
+            <Link to="/project" className="btn"> Projects
+            </Link>
+            
+            <Link to="/contact" className="btn btn-light"> Contact
+            </Link>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default HeroImg
--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-
+import Navbar from '../components/Navbar';
+import HeroImg from '../components/HeroImg';
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <HeroImg></HeroImg>
     </div>
   )
 }
