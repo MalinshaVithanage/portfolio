06fa5c5a584b6ced042a35ec10ee0aa75d92ba53
Malinsha Vithanage <kavindigvm.20@uom.lk>
2023-08-25 23:59:16 +0530

    projects finished

---
+++ my-app/src/components/WorkCardData.js
@@ -0,0 +1,18 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+
+const ProjectCardData=[
+    {
+        imgsrc: pro1,
+        title: "Arctic Internal Service Management System",
+        text: " A system for creating a user-friendly web application to manage internal services in the company, Primarily focusing on computerizing the process of creating jobs, site visits, employees databases and connecting the personnel of the company, which include the managers, technicians, call operators and drivers. Technologies: Angular | Spring Boot | My SQL | TypeScript",
+        source:" https://github.com/Manipulus2023/Manipulus" 
+    },
+    {
+        imgsrc: pro2,
+        title: "POS system",
+        text: " The frontend of a POS system for a restaurant using React JS. I built a responsive and user-friendly UI that was easy for restaurant staff to use. I integrated the frontend with the backend API to allow for real-time data updates. Technologies: React JS",
+        source:"https://github.com/MalinshaVithanage/POS" 
+    },
+];
+ export default ProjectCardData;
