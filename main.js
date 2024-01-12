52330cf6e6f3e98c08d6fab037a6d89551b12996
Malinsha Vithanage <kavindigvm.20@uom.lk>
2024-01-12 19:49:54 +0530

    First Part Done

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Tech Enthusiast", "Full-stack Developer", "Volunteer"], 
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  
